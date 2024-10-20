edea6cbc139b8861964f84a1c62fa57a69eddc8d
TurobSulaymonov <turobsulaymonov117@gmail.com>
2024-10-20 21:40:06 +0900

    feat: develop authtication page and design ui pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,11 @@ import SearchFillIcon from "./ui/icons/SearchFillIcon";
 import SearchIcon from "./ui/icons/SearchIcon";
 import {usePathname} from "next/navigation";
 import NewFillIcon from "./ui/icons/NewFillIcon";
- 
+import ColorButton from "./ui/icons/ColorButton";
+import { useSession, signIn, signOut } from "next-auth/react"
+import Avatar from "./Avatar";
+
+
 const menu = [
     {
         href: '/', icon: <HomeIcon/>, 
@@ -28,25 +32,39 @@ const menu = [
 
  export default function Navbar() {
     const pathName = usePathname();
+    const {data: session} =useSession();
+    const user =session?.user;
     return (
-        <div>
+        <div className="flex justify-between items-center px-6">
             <Link href="/">
-            <h1>Bookgram</h1>
+            <h1 className="text-3xl font-bold">Bookgram</h1>
             </Link>
             <nav>
-                <ul>
+                <ul className="flex gap-4 items-center p-4">
                     {
-                      menu.map(item => 
-                        
-                       <li key={item.href} >
+                      menu.map(item => (
+                          <li key={item.href} >
                           <Link href={item.href}>
                           {pathName === item.href ? item.clickedIcon : item.icon}
                           </Link>
                         </li>
                     
+                    ))}
+                 {user && (
+                       <li>
+                       <Link href={`/user/${user.username}`}></Link>
+                        <Avatar image={user.image} /> 
+                   </li>
+                 )}
+                 <li>
+                 {
+                    session ? (
+                        <ColorButton text="Sign out" onClick={() => signOut()} />
+                    ) : (
+                        <ColorButton text="Sign in" onClick={() => signIn()} />
                     )
-                    }
-
+                  }
+                 </li>
                 </ul>
             </nav>
         </div>
